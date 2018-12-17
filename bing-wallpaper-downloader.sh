9f4d91de81b38e8e7b7f31e0029d9577d19de501
lelouch <lelouch.279978955@gmail.com>
2018-12-17 09:13:29 +0800

    1. 修复了两个个bug:     在下载图片的时候是使用当前时间(精确到miao)作为图片名。但是bing每天只有一张图片，所以如果一天多次下载的话会产生重复图片，把时间改为精确到天就好了。     Mac和Linux下的grep工具不同，Mac下应该使用ggrep，但是在检测操作系统时错误指定了-a参数，现在改为-s参数。先前没有报错是因为我只是指定了ggrep而没有分OS指定工具，而且我没有在Linux试过该脚本。 2. 添加了一个新功能：     由用户指定具体日期，来下载该日的图片。具体是在命令行中指定一个参数表示距离今天多少天，0表示当天，1表示前一条，以此类推。 3. 还有需要做的：     对于日期的指定，似乎超过一定数目就会无效，后面会找出原因并且修复。

--- bing-wallpaper-downloader.sh
+++ bing-wallpaper-downloader.sh
@@ -4,42 +4,53 @@
 PICTURE_DIR="$HOME/Pictures/bing-wallpapers"
 # 必应网址
 BING="http://www.bing.com"
-# 获取xml数据
-# 其中idx值决定从何时开始，0表示当日，1表示前一天
-xmlURL="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-WW"
 # 图片分辨率
 picResolution="_1920x1200"
 # 图片后缀
 picExtension=".jpg"
 
-fileName=$(date +%Y%m%d%H%M%S)$picExtension
+# 输入的时间偏移量，0表示当日，1表示前一天
+if [ -n "$2" ]; then
+    offset="0"
+elif [[ $1 -lt 0 ]]; then
+    echo "参数必须为非负数"
+    exit 1
+else
+    offset=$1
+fi
+# 由于每天只有一张图片
+# 所以可以根据日期来对图片命名
+day=$(( $(date +%d) - $offset ))
+if [[ $day -lt 10 ]]; then
+    day="0"$day
+fi
+fileName=$(date +%Y%m)$day$picExtension
+# echo "fileName: $fileName"
+
+# 获取xml数据
+# 其中idx值决定从何时开始，0表示当日，1表示前一天
+xmlURL="$BING/HPImageArchive.aspx?format=xml&idx=$offset&n=1&mkt=en-WW"
 
 # 根据操作系统类型确定grep工具
-# 由于Mac自带的grep命令不支持perl(-p)
+# 由于Mac自带的grep命令不支持perl(-P)
 # 所以需要使用brew下载grep工具
 # 但是一般不覆盖原生的grep，所以取名为ggrep
-OS=$(uname -a)
-if [ "$OS" =  "Linux" ]; then
+OS=$(uname -s)
+if [ "$OS" ==  "Linux" ]; then
     greptool="grep"
-elif [ "$OS" = "Darwin" ]; then
+elif [ "$OS" == "Darwin" ]; then
     greptool="ggrep"
 fi
 
 
-# picURL=$BING$(echo $(curl -s "$xmlURL") | $greptool -oP "<urlBase>(.*)</urlBase>" | cut -d ">" -f 2 | cut -d "<" -f 1)"$picResolution""$picExtension"
-
-# 使用perl正则表达式
-picURL=$BING$(echo $(curl -s "$xmlURL") | ggrep -oP '(?<=<urlBase>).*(?=</urlBase>)')$picResolution$picExtension
+# 获取图片的url
+# 这里使用了perl正则表达式
+picURL=$BING$(echo $(curl -s "$xmlURL") | $greptool -oP '(?<=<urlBase>).*(?=</urlBase>)')$picResolution$picExtension
 
 # echo "picURL: $picURL"
-# echo "fileName: $fileName"
-
-
 
 # 检测目录是否存在，不存在则创建
-if [ -e "$PICTURE_DIR" ]; then
-    echo "目录存在，直接下载图片..."
-else
+if [ ! -e "$PICTURE_DIR" ]; then
     echo "目录不存在，创建目录: $PICTURE_DIR"
     mkdir -p "$PICTURE_DIR"
 fi
@@ -49,6 +60,4 @@ echo "下载图片... "
 curl -s -o "$PICTURE_DIR/$fileName" "$picURL"
 echo "图片下载完成， 保存至$PICTURE_DIR/$fileName"
 
-exit
-
-# TODO: 可以指定日期下载图片
+exit 0
